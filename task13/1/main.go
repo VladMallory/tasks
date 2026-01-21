b569e3a7f0f91d3edb33d3f24638efa60a9b70e7
Владислав Коваенко Анатольевич <vladmallory0321@gmail.com>
2026-01-21 15:52:12 +1000

    Задания

--- task13/1/main.go
+++ task13/1/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"sync"
 	"time"
 )
 
@@ -35,8 +37,58 @@ func (n NetworkFetcher) Do(ctx context.Context) error {
 }
 
 func main() {
-	//f1 := FileProcessor{}
-	//
-	//n1 := NetworkFetcher{}
+	var wg sync.WaitGroup
+	wg.Add(2)
 
+	go func() {
+		defer wg.Done()
+		da1()
+	}()
+
+	go func() {
+		defer wg.Done()
+		da2()
+	}()
+
+	wg.Wait()
+
+}
+
+func da1() {
+
+	// закрываю соединение
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	// Воркер
+	var worker Worker
+
+	// FileProcessor
+	worker = FileProcessor{}
+
+	// Обращаюсь к интерфейсу и его
+	// методу do, даю туда контекс
+	// Так мы привязываем это к интерфейсу и можно делать
+	// подмену реализаций метода Do()
+	if err := worker.Do(ctx); err != nil {
+		fmt.Println(err)
+	} else {
+		// Поскольку ошибка nil, будет успешно
+		fmt.Println("FileProcessor: успешно")
+	}
+}
+
+// Это для эксперемента. Поскольку службы между собой
+// не связаны, можно сделать асинхронно
+func da2() {
+	ctx2, cancel2 := context.WithTimeout(context.Background(), 2*time.Second)
+	// закрываю соединение
+	defer cancel2()
+
+	worker := NetworkFetcher{}
+	if err := worker.Do(ctx2); err != nil {
+		fmt.Println(err)
+	} else {
+		// Поскольку ошибка nil, будет успешно
+		fmt.Println("NetworkFetcher: успешно")
+	}
 }
