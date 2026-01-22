6b98b435f0557a45fef98d8370c8636576e0e60f
Владислав Коваенко Анатольевич <vladmallory0321@gmail.com>
2026-01-23 02:33:01 +1000

    Задания

---
+++ task17/1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func ConvertIntsToInterface(ints []int) []interface{} {
+	result := make([]interface{}, len(ints))
+
+	// складывает в result все что получил из ints
+	for i, v := range ints {
+		result[i] = v
+	}
+
+	// и возвраем итоговый слайс. А поскольку у нас
+	// []interface (или any), можно положить слайс
+	return result
+}
+
+func main() {
+	slice := []int{1, 2, 4, 5, 5, 100, 2}
+
+	// []int и interface это разные типы. Мы не можем напрямую
+	// их преобразовать
+	da := ConvertIntsToInterface(slice)
+	fmt.Println(da)
+}
