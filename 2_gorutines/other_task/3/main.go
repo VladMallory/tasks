d446813c7ff34b8c7fb799d192e12e6b8c309aed
Владислав Коваенко Анатольевич <vladmallory0321@gmail.com>
2026-02-05 19:03:43 +1000

    Задания

--- 2_gorutines/other_task/3/main.go
+++ 2_gorutines/other_task/3/main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker(id int, jobs <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for job := range jobs {
+		fmt.Println(id, job)
+
+		time.Sleep(300 * time.Millisecond)
+	}
+}
+
+func main() {
+	jobs := make(chan int)
+
+	var wg sync.WaitGroup
+
+	const workers = 3
+
+	wg.Add(workers)
+
+	for i := 1; i <= workers; i++ {
+		go worker(i, jobs, &wg)
+	}
+
+	// Продюсер
+	for j := 1; j <= 20; j++ {
+		jobs <- j
+	}
+
+	close(jobs)
+
+	wg.Wait()
+}
